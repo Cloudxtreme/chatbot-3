cb58938c8292e77f47b4a0aa48525eab6d9e165f
Bandhan Munjal <bamunjal1995@gmail.com>
2014-12-05 16:12:27 +0530

    got both engines working
    metrix engine working
    never out of words

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -26,9 +26,15 @@ namespace chatbot
         string myname = "LUCIBOT";
         string urname;
         int flag;
+        bool found = false;
         string reply;
         string[] starterssent={ "hey there", "hello lucibot", "hi lucibot","hey lucibot" };
-        string[] complements={ "same here", "thank you" };
+        string[] queswords={ "who","you","produced","introduce","what","producer","?"};
+        string[] quessent={"who made you","who produced you","who are you","introduce yourself","what is your name","your name"};
+        string[] userwords = { "i", "me", "made", "my", "name","produced","?" };
+        string[] usersent = { "who made me", "who am i", "who produced me", "what is my name", "my name" };
+        string[] complementssent={ "same here", "thank you","you are awesome" };
+        string[] complementswords = { "same", "thank","thanks","awesome","wow","cool" };
         string[] starterswords = { "hey", "hii", "hi", "hello" };
         bool istyping = false;
         int askedname = 0;
@@ -115,42 +121,200 @@ namespace chatbot
 
         private void specialengine()
         {
-            int gothere = 0;
-              for (int i = 0; i < starterssent.Length; i++)
-                if (input.Text == starterssent[i])
-                {
-                    startings();
-                    gothere=1;
-                }
-             for (int i = 0; i < complements.Length; i++)
-                if (input.Text == complements[i])
-                {
-                    gothere=1;
-                complementing();
-                }
-             if (gothere == 0)
+            found = false;
+            if (found == false)
+            {
+                for (int i = 0; i < starterssent.Length; i++)
+                    if (input.Text == starterssent[i])
+                    {
+                        found = true;
+                        startings();
+                    }
+            }
+            if (found == false)
+            {
+                for (int i = 0; i < complementssent.Length; i++)
+                    if (input.Text == complementssent[i])
+                    {
+                        complementing();
+                        found = true;
+                    }
+            }
+            if (found == false)
+            {
+                for (int i = 0; i < quessent.Length; i++)
+                    if (input.Text == quessent[i])
+                    {
+                        found = true;
+                        intro();
+                    }
+            }
+            if (found == false)
+            {
+                for (int i = 0; i < usersent.Length; i++)
+                    if (input.Text == usersent[i])
+                    {
+                        found = true;
+                        outro();
+                    }
+            }
+             if (found==false)
                  matrixengine();
         }
 
         private void matrixengine()
         {
-            for (int i = 0; i < starterswords.Length; i++)
-                if (input.Text == starterswords[i])
-                    startings();
+            string[] words = input.Text.Split(' ');
+            if (found == false)
+            {
+                for (int i = 0; i < complementswords.Length; i++)
+                    if (words.Contains(complementswords[i]))
+                    {
+                        found = true;
+                        complementing();
+                    }
+            }
+            if (found == false)
+            {
+                for (int i = 0; i < starterswords.Length; i++)
+                    if (words.Contains(starterswords[i]))
+                    {
+                        found = true;
+                        startings();
+                    }
+            }
+            if (found == false)
+            {
+                for (int i = 0; i < queswords.Length; i++)
+                    if (words.Contains(queswords[i]))
+                    {
+                        found = true;
+                        intro();
+                    }
+            }
+            if (found == false)
+            {
+                for (int i = 0; i < userwords.Length; i++)
+                    if (words.Contains(userwords[i]))
+                    {
+                        found = true;
+                        outro();
+                    }
+            }
+            if(found==false)
+            outofbox();
+        }
 
+        private void outro()
+        {
+            int ch = rnd.Next(0, 4);
+            if (askedname == 0)
+                startings();
+            else
+            {
+                switch (ch)
+                {
+                    case 0:
+                        reply = "Let me guess..You are " + urname + " right?? humans made by god..hmm";
+                        timer.Start();
+                        break;
+                    case 1:
+
+                        reply = "God Made you..I think you are " + urname;
+                        timer.Start();
+                        break;
+                    case 2:
+                        reply = "Wait wait he told me,you got to be " + urname;
+                        timer.Start();
+                        break;
+                    case 3:
+                        reply = "We just discussed,you are " + urname+" A God's production";
+                        timer.Start();
+                        break;
+                }
+            }
+        }
+        private void outofbox()
+        {
+            if (askedname == 0)
+                startings();
+            int ch = rnd.Next(0, 4);
+            switch (ch)
+            {
+                case 0:
+                    reply = "I need a God's Dictionary for that...!!!";
+                    timer.Start();
+                    break;
+                    break;
+                case 1:
+                    reply = "Oops,thats currently out of my dictionary.try something else";
+                    timer.Start();
+                    break;
+                case 2:
+                    reply = "That Sounds Russian to ME..Is it so??";
+                    timer.Start();
+                    break;
+                case 3:
+                    reply = "Oh My God..I lost my intelligence...";
+                    timer.Start();
+                    break;
+            }
+        }
+        private void intro()
+        {
+            int ch = rnd.Next(0, 4);
+            switch (ch)
+            {
+                case 0:
+                    reply = "Oh!!My name is LuciBot-Made by Lucifer(Bandhan)";
+                    timer.Start();
+                    break;
+                case 1:
+                    reply = "Lucifer made me and named me LuciBot";
+                    timer.Start();
+                    break;
+                case 2:
+                    reply = "I am a cool bot named Lucibot";
+                    timer.Start();
+                    break;
+                case 3:
+                    reply = "I am an AI bot(LuciBot),Intelligent then humans";
+                    timer.Start();
+                    break;
+            }
         }
         private void startings()
         {
             if (askedname == 0)
             {
-                reply = "What is Your Name ?";
+                
+                int ch = rnd.Next(0,4);
+                switch (ch)
+                {
+                    case 0:
+                        reply = "Oh!What is your name?";
+                        timer.Start();
+                        break;
+                    case 1:
+                        reply = "i didn't Got your name..what was that?";
+                        timer.Start();
+                        break;
+                    case 2:
+                        reply = "Lets start with your name first..";
+                        timer.Start();
+                        break;
+                    case 3:
+                        reply = "This memory deleted your name by mistake..What is it?";
+                        timer.Start();
+                        break;
+                }
                 askedname = 1;
                 getname();
                 timer.Start();
             }
             if (askedname == 3)
             {
-                int ch = rnd.Next(0,3);
+                int ch = rnd.Next(0,4);
                 switch(ch)
                 { 
                     case 0:
@@ -158,11 +322,15 @@ namespace chatbot
                 timer.Start();
                 break;
                     case 1:
-                reply = "Heyyyyaaaa";
+                reply = "Heyyyyaaaa "+urname;
                 timer.Start();
                 break;
                     case 2:
-                reply = "hii there";
+                reply = "hii "+urname;
+                timer.Start();
+                break;
+                    case 3:
+                reply = "lets talk about something else " + urname;
                 timer.Start();
                 break;
                  }
@@ -173,7 +341,7 @@ namespace chatbot
      
           private void complementing()
         {
-            int ch = rnd.Next(0,3);
+            int ch = rnd.Next(0,5);
               switch(ch)
               { 
                   case 0:
@@ -186,6 +354,14 @@ namespace chatbot
                    break;
                   case 2:
                    reply = "Cool...";
+                   timer.Start();
+                   break;
+                  case 3:
+                   reply = "Me too";
+                   timer.Start();
+                   break;
+                  case 4:
+                   reply = "Oh come on..Thats fine";
                    timer.Start();
                    break;
 
